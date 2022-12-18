77f97eddad712f7132a152ac13d62a734d015c3f
CHOE BYEONGSEON <qudtjs3012@naver.com>
2022-12-19 00:42:05 +0900

    1.2 Hello servlet

---
+++ src/main/java/hello/servlet/basic/HelloServlet.java
@@ -0,0 +1,32 @@
+package hello.servlet.basic;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "helloServlet", urlPatterns = "/hello")  // name과 urlPatterns은 중복 허용x
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        System.out.println("HelloServlet.service");
+        System.out.println("request = " + request);
+        System.out.println("response = " + response);
+
+        String username = request.getParameter("username"); // URL에 있는 쿼리 파라미터 조회 가능
+        System.out.println("username = " + username);
+
+        response.setContentType("text/plain");
+        response.setCharacterEncoding("utf-8");
+        response.getWriter().write("hello " + username);
+    }
+}
+
+/*
+HttpServletRequest request - 유저가 요청한것을 서버에 던짐
+HttpServletResponse response - 서버에서 응답을 만들어 HTTP에 표시해 보여줌
+*/
