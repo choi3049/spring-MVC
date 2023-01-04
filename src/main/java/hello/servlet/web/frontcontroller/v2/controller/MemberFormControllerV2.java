7a49967ae457752b8df1c0314ca5c85b9eee28a8
CHOE BYEONGSEON <qudtjs3012@naver.com>
2023-01-04 23:58:25 +0900

    3.4 View_V3

--- src/main/java/hello/servlet/web/frontcontroller/v2/controller/MemberFormControllerV2.java
+++ src/main/java/hello/servlet/web/frontcontroller/v2/controller/MemberFormControllerV2.java
@@ -1,9 +1,8 @@
 package hello.servlet.web.frontcontroller.v2.controller;
 
-import hello.servlet.web.frontcontroller.v1.MyView;
+import hello.servlet.web.frontcontroller.MyView;
 import hello.servlet.web.frontcontroller.v2.ControllerV2;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
--- src/main/java/hello/servlet/web/frontcontroller/v2/controller/MemberSaveControllerV2.java
+++ src/main/java/hello/servlet/web/frontcontroller/v2/controller/MemberSaveControllerV2.java
@@ -2,10 +2,9 @@ package hello.servlet.web.frontcontroller.v2.controller;
 
 import hello.servlet.domain.member.Member;
 import hello.servlet.domain.member.MemberRepository;
-import hello.servlet.web.frontcontroller.v1.MyView;
+import hello.servlet.web.frontcontroller.MyView;
 import hello.servlet.web.frontcontroller.v2.ControllerV2;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
