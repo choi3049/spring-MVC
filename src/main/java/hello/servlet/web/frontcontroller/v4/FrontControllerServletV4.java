61e96ecef8f73a7d185cb95e4efab1e7cb5e6f9f
CHOE BYEONGSEON <qudtjs3012@naver.com>
2023-01-06 11:27:57 +0900

    3.5 V4

--- src/main/java/hello/servlet/web/frontcontroller/v4/FrontControllerServletV4.java
+++ src/main/java/hello/servlet/web/frontcontroller/v4/FrontControllerServletV4.java
@@ -25,36 +25,33 @@ public class FrontControllerServletV4 extends HttpServlet {
         controllerMap.put("/front-controller/v4/members", new MemberListControllerV4());
     }
 
+
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        System.out.println("FrontControllerServletV4.service");
-
         String requestURI = request.getRequestURI();
-
         ControllerV4 controller = controllerMap.get(requestURI);
+
         if (controller == null) {
             response.setStatus(HttpServletResponse.SC_NOT_FOUND);
             return;
         }
 
-        Map<String, String> paramMap = createParamMap(request);
-        Map<String, Object> model = new HashMap<>(); //추가
+        Map<String, String> paramMap = createParamMap(request); Map<String, Object> model = new HashMap<>(); //추가
         String viewName = controller.process(paramMap, model);
-
         MyView view = viewResolver(viewName);
-
         view.render(model, request, response);
     }
 
-    private MyView viewResolver(String viewName) {
-
-        return new MyView("/WEB-INF/views/" + viewName + ".jsp");
-    }
 
     private Map<String, String> createParamMap(HttpServletRequest request) {
         Map<String, String> paramMap = new HashMap<>();
-        request.getParameterNames().asIterator().
-                forEachRemaining(paramName -> paramMap.put(paramName, request.getParameter(paramName)));
-        return paramMap;
+        request.getParameterNames().asIterator()
+                   .forEachRemaining(paramName -> paramMap.put(paramName,
+            request.getParameter(paramName)));
+          return paramMap;
+    }
+
+    private MyView viewResolver(String viewName) {
+        return new MyView("/WEB-INF/views/" + viewName + ".jsp");
     }
 }
